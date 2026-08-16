89c3e7f47ecc21710c80c239f4fd724b367970d6
eunchae791031-afk <eunchae791031@gmail.com>
2026-08-16 21:24:49 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PostypeArchive from "./PostypeArchive";
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PostypeArchive />
+  </React.StrictMode>
+);
